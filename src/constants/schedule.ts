b86994b37be35f9113f1b3b176ea45219308d4f3
Thân <minhthan007@gmail.com>
2025-02-03 12:40:46 +0700

    release auto

--- src/constants/schedule.ts
+++ src/constants/schedule.ts
@@ -2,27 +2,9 @@ import { Schedule } from "@/types";
 
 export const schedules: Schedule[] = [
   {
-    channelName: "VTV1",
-    hour: 0,
-    dayOfWeek: 0,
-    fptplay: 1,
-  },
-  {
-    channelName: "VTV2",
-    hour: 0.25,
-    dayOfWeek: 0,
-    fptplay: 1,
-  },
-  {
-    channelName: "VTV3",
-    hour: 0.5,
-    dayOfWeek: 0,
-    fptplay: 1,
-  },
-  {
-    channelName: "VTV4",
-    hour: 0.75,
-    dayOfWeek: 0,
-    fptplay: 1,
+    channelName: "Ha Noi 1",
+    hour: 12.5,
+    dayOfWeek: 1,
+    fptplay: 30,
   },
 ];
