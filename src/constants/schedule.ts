61023a1348d6178324fac5a144bf29f50f413a89
Thân <minhthan007@gmail.com>
2025-02-04 19:43:18 +0700

    release auto

--- src/constants/schedule.ts
+++ src/constants/schedule.ts
@@ -2,9 +2,15 @@ import { Schedule } from "@/types";
 
 export const schedules: Schedule[] = [
   {
-    channelName: "Ha Noi 1",
+    channelName: "Hà Nội 2",
     hour: 12.5,
     dayOfWeek: 1,
     fptplay: 30,
   },
+  {
+    channelName: "HTVC Du lịch",
+    hour: 20,
+    dayOfWeek: 1,
+    fptplay: 106,
+  }
 ];
